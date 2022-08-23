67a4f9b964e4c08fd6855e94aafbd5ad339d06b7
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-08-23 16:01:48 +0500

    Add full names for doctors

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Character.create(name: "Luke", movie: movies.first)
 # User.create!(username:  "Example User", password: "password", password_confirmation: "password")
 
-@doctor1 = Doctor.create!(name: 'Doctor1', specialization: 'eye specialist', available_times: 'monday-friday', hourly_rate: 250, picture: ('https://asset.cloudinary.com/dcphbr3ka/607f68c350ebf6725e2804f73eba01bf'))
+@doctor1 = Doctor.create!(name: 'Nandita Sarchand', specialization: 'eye specialist', available_times: 'monday-friday', hourly_rate: 250, picture: ('https://asset.cloudinary.com/dcphbr3ka/607f68c350ebf6725e2804f73eba01bf'))
 
 
-@doctor2 = Doctor.create!(name: 'Doctor2', specialization: 'eye specialist', available_times: 'monday-friday', hourly_rate: 250, picture: ('https://asset.cloudinary.com/dcphbr3ka/87e6862dcd2ddc1f58c293f7c999c534'))
+@doctor2 = Doctor.create!(name: 'Ellen Abel', specialization: 'eye specialist', available_times: 'monday-friday', hourly_rate: 250, picture: ('https://asset.cloudinary.com/dcphbr3ka/87e6862dcd2ddc1f58c293f7c999c534'))
 
-@doctor3 = Doctor.create!(name: 'Doctor3', specialization: 'eye specialist', available_times: 'monday-friday', hourly_rate: 250, picture: ('https://asset.cloudinary.com/dcphbr3ka/6ca49db87b0e6d8ed3ba05f84cf43472'))
+@doctor3 = Doctor.create!(name: 'Girard Geoni', specialization: 'eye specialist', available_times: 'monday-friday', hourly_rate: 250, picture: ('https://asset.cloudinary.com/dcphbr3ka/6ca49db87b0e6d8ed3ba05f84cf43472'))
 
