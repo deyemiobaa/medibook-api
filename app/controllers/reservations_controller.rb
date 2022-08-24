f91e1baa8227968e3e1687e30ab7a612c14057ca
sainingo <nickmentor421@gmail.com>
2022-08-24 12:11:26 +0300

    Create actions for reservation controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,10 +1,29 @@
 class ReservationsController < ApplicationController
   def index
+    @reservations = Reservation.all
+    render json: @reservations
+  end
+
+  def new
+    @reservations = Reservation.new
   end
 
   def create
+    @reservations = Reservation.create(reservation_params)
+    @reservations.user = current_user
+    if @reservations.save
+      redirect_to @reservations
+    else
+      render :new
+    end
   end
 
   def show
+    @reservations = Reservation.find(params[:id])
+  end
+
+  private
+  def reservation_params
+    params.require(:reservations).permit(:date, :duration, :total)
   end
 end
