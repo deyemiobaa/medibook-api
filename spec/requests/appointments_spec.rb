9fbd8ba6a08ed0ee9f37a666a093afca1bbab2f3
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-30 22:45:21 +0100

    appointments spec

--- spec/requests/appointments_spec.rb
+++ spec/requests/appointments_spec.rb
@@ -1,24 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe 'appointments', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/appointments/index'
-      expect(response).to have_http_status(:success)
+RSpec.describe 'Appointments', type: :request do
+  before do
+    @doctor = FactoryBot.create(:doctor)
+    @current_user = FactoryBot.create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@current_user)
+    allow_any_instance_of(ApplicationController).to receive(:authenticate_request).and_return(true)
+  end
+
+  describe 'GET /appointments' do
+    it 'returns all appointments' do
+      get appointments_path
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body).size).to eq(0)
     end
   end
 
-  describe 'GET /create' do
-    it 'returns http success' do
-      get '/appointments/create'
-      expect(response).to have_http_status(:success)
+  describe 'POST /appointments' do
+    it 'creates a appointment' do
+      expect {
+        post appointments_path, params: { appointment: { date: '2020-01-01', duration: '1', doctor_id: @doctor.id } }
+      }.to change(Appointment, :count).by(1)
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/appointments/show'
-      expect(response).to have_http_status(:success)
+  describe 'DELETE /appointments/:id' do
+    it 'deletes a appointment' do
+      @appointment = FactoryBot.create(:appointment, doctor: @doctor, user: @current_user)
+      expect {
+        delete appointment_path(@appointment)
+      }.to change(Appointment, :count).by(-1)
     end
   end
 end
