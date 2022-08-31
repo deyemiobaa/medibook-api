dc98c46a0d90ec10b8c6d93887d5571524c0de28
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-31 19:59:45 +0100

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,9 @@
 #   Character.create(name: "Luke", movie: movies.first)
 # User.create!(username:  "Example User", password: "password", password_confirmation: "password")
 
-@doctor1 = Doctor.create!(name: 'Nandita Sarchand', specialization: 'eye specialist', available_times: 'monday-friday', hourly_rate: 250, picture: ('https://asset.cloudinary.com/dcphbr3ka/607f68c350ebf6725e2804f73eba01bf'))
+@doctor1 = Doctor.create!(name: 'Dr. Khadijah Zaima', specialization: 'eye specialist', available_times: 'monday-friday', hourly_rate: 250, picture: ('https://res.cloudinary.com/deyemiobaa/image/upload/v1661971456/medi-book/khadijah-zaima.jpg'))
 
+@doctor3 = Doctor.create!(name: 'Dr. Katherine Powells', specialization: 'Opthamology', available_times: 'Monday, Friday, and Saturday', hourly_rate: 250, picture: ('https://res.cloudinary.com/deyemiobaa/image/upload/v1661971056/medi-book/katherine-powells-1n.jpg'))
 
-@doctor2 = Doctor.create!(name: 'Ellen Abel', specialization: 'eye specialist', available_times: 'monday-friday', hourly_rate: 250, picture: ('https://asset.cloudinary.com/dcphbr3ka/87e6862dcd2ddc1f58c293f7c999c534'))
-
-@doctor3 = Doctor.create!(name: 'Girard Geoni', specialization: 'eye specialist', available_times: 'monday-friday', hourly_rate: 250, picture: ('https://asset.cloudinary.com/dcphbr3ka/6ca49db87b0e6d8ed3ba05f84cf43472'))
+@doctor2 = Doctor.create!(name: 'Dr. Isaac Collins', specialization: 'Therapy', available_times: 'Monday - Friday', hourly_rate: 150, picture: ('https://res.cloudinary.com/deyemiobaa/image/upload/v1661970752/medi-book/isaac-w-collins-1_kwuihq.jpg'))
 
